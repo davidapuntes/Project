6c05f28bd107fc2098d4741be69c406e7f7c7af2
David Martín-Serrano Narváez <davidapuntes@hotmail.com>
2019-09-14 19:10:38 +0200

    Reverted Register

--- PresupuesteaSolution/Presupuestea/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PresupuesteaSolution/Presupuestea/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -8,11 +8,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
-using Presupuestea.Data.Interfaces;
 using Presupuestea.Data.Model;
-using System.Linq;
 
 namespace Presupuestea.Areas.Identity.Pages.Account
 {
@@ -23,46 +20,21 @@ namespace Presupuestea.Areas.Identity.Pages.Account
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
-        private ICategoryRepository _repoCategorias;
-
-
-        private List<Category> _listaCategorias;
-
 
         public RegisterModel(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender,
-            ICategoryRepository repoCategorias
-)
+            IEmailSender emailSender)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
-            _repoCategorias = repoCategorias;
-
-
-            //Rellenamos las categorías de los profesionales
-            _listaCategorias = repoCategorias.GetAll().ToList();
-            this.ContractorInputModel = new ContractorInput();
-            this.ContractorInputModel.Categories = _listaCategorias.ConvertAll(a =>
-            {
-                return new SelectListItem()
-                {
-                    Text = a.Name,
-                    Value = a.CategoryID.ToString(),
-                    Selected = false
-                };
-            });
-
         }
 
         [BindProperty]
-        public CustomerInput CustomerInputModel { get; set; }
-        [BindProperty]
-        public ContractorInput ContractorInputModel { get; set; }
+        public InputModel Input { get; set; }
 
         public string ReturnUrl { get; set; }
 
@@ -88,20 +60,10 @@ namespace Presupuestea.Areas.Identity.Pages.Account
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
         }
-        public class CustomerInput : InputModel { }
 
-        public class ContractorInput : InputModel
-        {                 
-
-            [Required]
-            [Display(Name = "Categoría")]
-            public string SelectedCategory { get; set; }
-            public IEnumerable<SelectListItem> Categories { get; set; }
-        }
         public void OnGet(string returnUrl = null)
         {
             ReturnUrl = returnUrl;
-
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
@@ -109,8 +71,8 @@ namespace Presupuestea.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = CustomerInputModel.Email, Email = CustomerInputModel.Email, PostalCode = CustomerInputModel.PostalCode };
-                var result = await _userManager.CreateAsync(user, CustomerInputModel.Password);
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email , PostalCode = Input.PostalCode};
+                var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
@@ -122,7 +84,7 @@ namespace Presupuestea.Areas.Identity.Pages.Account
                         values: new { userId = user.Id, code = code },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(CustomerInputModel.Email, "Confirm your email",
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
